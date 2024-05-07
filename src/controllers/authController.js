a2f0fca2a58e0aec9fe9ff6cb43879fd7dbdc713
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-05-07 12:29:07 +0700

    handle login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -43,6 +43,32 @@ const register = asyncHandle(async (req, res) => {
     },
   });
 });
+
+const login = asyncHandle(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await UserModel.findOne({ email });
+  if (!existingUser) {
+    res.status(403).json({
+      message: "User not found!!!",
+    });
+    throw new Error("User not found !!!");
+  }
+  const isMatchPassword = await bcrypt.compare(password, existingUser.password);
+  if (!isMatchPassword) {
+    res.status(401);
+    throw new Error("Email or Password is not correct!");
+  }
+  res.status(200).json({
+    message: "Login successfully",
+    data: {
+      id: existingUser.id,
+      email: existingUser.email,
+      accesstoken: await getJsonWebToken(email, existingUser.id),
+    },
+  });
+});
+
 module.exports = {
   register,
+  login,
 };
--- src/routers/authRoute.js
+++ src/routers/authRoute.js
@@ -1,8 +1,8 @@
 const Router = require("express");
-const { register } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
-
+authRouter.post("/login", login);
 module.exports = authRouter;
