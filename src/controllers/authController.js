408f4af6a55c565f79bfac14db66fca404d3b5ba
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-06-13 00:08:19 +0700

    fix authcontroller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
+/** @format */
+
 const UserModel = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcryp = require("bcrypt");
 const asyncHandle = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
@@ -8,7 +10,6 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
     user: process.env.USERNAME_EMAIL,
     pass: process.env.PASSWORD_EMAIL,
@@ -30,7 +31,8 @@ const getJsonWebToken = async (email, id) => {
 const handleSendMail = async (val) => {
   try {
     await transporter.sendMail(val);
-    return "OKE";
+
+    return "OK";
   } catch (error) {
     return error;
   }
@@ -38,17 +40,20 @@ const handleSendMail = async (val) => {
 
 const verification = asyncHandle(async (req, res) => {
   const { email } = req.body;
+
   const verificationCode = Math.round(1000 + Math.random() * 9000);
 
   try {
     const data = {
-      from: `Support Eventhub Application👻 <xuankhanguyen.hcmute@gmail.com>`, // sender address
-      to: email, // list of receivers
-      subject: "Verification email code ✔", // Subject line
-      text: "Your code to verification email", // plain text body
-      html: `<h2>Your verification code is: ${verificationCode}</h2>`, // html body
+      from: `"Support EventHub Appplication" <${process.env.USERNAME_EMAIL}>`,
+      to: email,
+      subject: "Verification email code",
+      text: "Your code to verification email",
+      html: `<h1>${verificationCode}</h1>`,
     };
+
     await handleSendMail(data);
+
     res.status(200).json({
       message: "Send verification code successfully!!!",
       data: {
@@ -67,25 +72,26 @@ const register = asyncHandle(async (req, res) => {
   const existingUser = await UserModel.findOne({ email });
 
   if (existingUser) {
-    res.status(401);
-    throw new Error(`Người dùng đã tồn tại!!`);
+    res.status(400);
+    throw new Error("User has already exist!!!");
   }
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const salt = await bcryp.genSalt(10);
+  const hashedPassword = await bcryp.hash(password, salt);
 
   const newUser = new UserModel({
     email,
     fullname: fullname ?? "",
     password: hashedPassword,
   });
+
   await newUser.save();
 
   res.status(200).json({
-    message: "Đăng ký người dùng thành công",
+    message: "Register new user successfully",
     data: {
       email: newUser.email,
-      id: newUser.id,
+      id: newUser._id,
       accesstoken: await getJsonWebToken(email, newUser.id),
     },
   });
@@ -93,27 +99,34 @@ const register = asyncHandle(async (req, res) => {
 
 const login = asyncHandle(async (req, res) => {
   const { email, password } = req.body;
+
   const existingUser = await UserModel.findOne({ email });
+
   if (!existingUser) {
-    res.status(403).json({
-      message: "User not found!!!",
-    });
-    throw new Error("User not found !!!");
+    res.status(403);
+    throw new Error("User not found!!!");
   }
-  const isMatchPassword = await bcrypt.compare(password, existingUser.password);
+
+  const isMatchPassword = await bcryp.compare(password, existingUser.password);
+
   if (!isMatchPassword) {
     res.status(401);
     throw new Error("Email or Password is not correct!");
   }
+
   res.status(200).json({
     message: "Login successfully",
     data: {
       id: existingUser.id,
       email: existingUser.email,
       accesstoken: await getJsonWebToken(email, existingUser.id),
+      fcmTokens: existingUser.fcmTokens ?? [],
+      photo: existingUser.photoUrl ?? "",
+      name: existingUser.name ?? "",
     },
   });
 });
+
 const forgotPassword = asyncHandle(async (req, res) => {
   const { email } = req.body;
 
@@ -129,8 +142,8 @@ const forgotPassword = asyncHandle(async (req, res) => {
 
   const user = await UserModel.findOne({ email });
   if (user) {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(`${randomPassword}`, salt);
+    const salt = await bcryp.genSalt(10);
+    const hashedPassword = await bcryp.hash(`${randomPassword}`, salt);
 
     await UserModel.findByIdAndUpdate(user._id, {
       password: hashedPassword,
@@ -162,13 +175,32 @@ const handleLoginWithGoogle = asyncHandle(async (req, res) => {
   const userInfo = req.body;
 
   const existingUser = await UserModel.findOne({ email: userInfo.email });
-  let user = { ...userInfo };
+  let user;
   if (existingUser) {
     await UserModel.findByIdAndUpdate(existingUser.id, {
-      ...userInfo,
       updatedAt: Date.now(),
     });
+    user = { ...existingUser };
     user.accesstoken = await getJsonWebToken(userInfo.email, userInfo.id);
+
+    if (user) {
+      const data = {
+        accesstoken: user.accesstoken,
+        id: existingUser._id,
+        email: existingUser.email,
+        fcmTokens: existingUser.fcmTokens,
+        photo: existingUser.photoUrl,
+        name: existingUser.name,
+      };
+
+      res.status(200).json({
+        message: "Login with google successfully!!!",
+        data,
+      });
+    } else {
+      res.sendStatus(401);
+      throw new Error("fafsf");
+    }
   } else {
     const newUser = new UserModel({
       email: userInfo.email,
@@ -176,12 +208,26 @@ const handleLoginWithGoogle = asyncHandle(async (req, res) => {
       ...userInfo,
     });
     await newUser.save();
+    user = { ...newUser };
     user.accesstoken = await getJsonWebToken(userInfo.email, newUser.id);
+
+    if (user) {
+      res.status(200).json({
+        message: "Login with google successfully!!!",
+        data: {
+          accesstoken: user.accesstoken,
+          id: user._id,
+          email: user.email,
+          fcmTokens: user.fcmTokens,
+          photo: user.photoUrl,
+          name: user.name,
+        },
+      });
+    } else {
+      res.sendStatus(401);
+      throw new Error("fafsf");
+    }
   }
-  res.status(200).json({
-    message: "Login with google successfully!!",
-    data: user,
-  });
 });
 
 module.exports = {
